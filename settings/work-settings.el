317a871b5e877000b2a7d517d33d02259eda2e29
Nick Lamberth <Nick.Lamberth@enginegroup.com>
2017-03-21 13:04:04 -0400

    adjust work settings for new job!

--- settings/work-settings.el
+++ settings/work-settings.el
@@ -1,24 +1,17 @@
 ;; work settings
 
 (setq exec-path
-      (append exec-path '("C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\" 
-			  "C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\Scripts\\"
-			  "C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\lib\\site-packages\\"
+      (append exec-path '("C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\" 
+			  "C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\Scripts\\"
+			  "C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\lib\\site-packages\\"
 			  "C:\\PortableGit\\cmd\\"
 			  "C:\\PortableGit\\bin\\"
-			  "C:\\Users\\nlambert1\\Desktop\\cmder\\bin\\"
-			  "C:\\clojure-1.8.0\\"
-			  "C:\\Program Files (x86)\\Java\\jre7\\bin\\"
-			  "C:\\pgsql\\bin"
 			  )))
 
 (setenv "PATH" (concat (getenv "PATH")
-		       ";C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\"
-		       ";C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\Scripts\\"
-		       ";C:\\WinPython-64bit-2.7.10.3\\python-2.7.10.amd64\\lib\\site-packages\\"
+		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\"
+		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\Scripts\\"
+		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\lib\\site-packages\\"
 		       ";C:\\PortableGit\\cmd\\"
-		       ";C:\\Users\\nlambert1\\Desktop\\cmder\\bin\\"
-		       ";C:\\clojure-1.8.0\\"
-		       ";C:\\Program Files (x86)\\Java\\jre7\\bin\\"
-		       ";C:\\pgsql\\bin"
+		       ";C:\\PortableGit\\bin\\"
 		       ))
