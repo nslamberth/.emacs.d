4b6ce7d59fd5d0e0e0f475d606729efc8ab7d98c
Nick Lamberth <nslamberth@gmail.com>
2017-03-24 17:17:59 -0400

    clean up work path, add google trends function

--- settings/work-settings.el
+++ settings/work-settings.el
@@ -15,3 +15,18 @@
 		       ";C:\\PortableGit\\cmd\\"
 		       ";C:\\PortableGit\\bin\\"
 		       ))
+
+;; fix some annoying python encoding problems
+(setenv "LANG" "en_US.UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LC_CTYPE" "en_US.UTF-8")
+(setenv "PYTHONIOENCODING" "utf-8")
+
+;; custom functions
+
+(defun google-trends-pull-report (keywords)
+  "Pull a google trend report and save result to current directory."
+  (interactive)
+  (let ((path (concat (expand-file-name "~") "/projects/google_trends/")))
+    (async-shell-command (format
+		    "python %smain.py %s" path keywords))))
