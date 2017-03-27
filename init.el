8539e8b331ef8025258c2b6e37744a8df405ad00
Nick Lamberth <nslamberth@gmail.com>
2017-03-27 12:02:17 -0400

    March 27 2017 Sync Up

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;; NICK LAMBERTH EMACS INIT
 
 ;; clean up window bars early
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
