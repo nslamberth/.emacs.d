d27304b79420b8254a8540a106e846869d8160e7
Nick Lamberth <nslamberth@gmail.com>
2017-04-02 23:13:44 -0400

    add ffmpeg mp3 command

--- settings/home-settings.el
+++ settings/home-settings.el
@@ -9,3 +9,18 @@
 
 (setq elpy-rpc-python-command "python3")
 
+;; misc commands
+
+(defun ffmpeg-convert-to-mp3 (file)
+  "Convert an audio file to 320k mp3."
+  (interactive (list (read-file-name "File to convert:")))
+  (async-shell-command
+  (string-join
+   (list
+    "ffmpeg" "-i"
+    (concat "\"" (expand-file-name file) "\"")
+    "-b:a 320k"
+    (concat "\""
+	    (replace-regexp-in-string
+	     "\\.\\w*$" ".mp3" (expand-file-name file "~/Desktop"))
+	    "\"")) " ")))
