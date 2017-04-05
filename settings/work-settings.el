3d05669d7591401430a44702252082807428a664
Nick Lamberth <nslamberth@gmail.com>
2017-04-05 09:36:37 -0400

    update python version

--- settings/work-settings.el
+++ settings/work-settings.el
@@ -1,17 +1,17 @@
 ;; work settings
 
 (setq exec-path
-      (append exec-path '("C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\" 
-			  "C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\Scripts\\"
-			  "C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\lib\\site-packages\\"
+      (append exec-path '("C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\" 
+			  "C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\Scripts\\"
+			  "C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\lib\\site-packages\\"
 			  "C:\\PortableGit\\cmd\\"
 			  "C:\\PortableGit\\bin\\"
 			  )))
 
 (setenv "PATH" (concat (getenv "PATH")
-		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\"
-		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\Scripts\\"
-		       ";C:\\WinPython-64bit-3.6.0.1Qt5\\python-3.6.0.amd64\\lib\\site-packages\\"
+		       ";C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\"
+		       ";C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\Scripts\\"
+		       ";C:\\WinPython-64bit-3.6.1.0Qt5\\python-3.6.1.amd64\\lib\\site-packages\\"
 		       ";C:\\PortableGit\\cmd\\"
 		       ";C:\\PortableGit\\bin\\"
 		       ))
