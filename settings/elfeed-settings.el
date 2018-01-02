b176caa700492275dd22177cf50d712d869d9820
Nick Lamberth <nslamberth@gmail.com>
2018-01-02 17:38:05 -0500

    add elfeed and elfeed config

---
+++ settings/elfeed-settings.el
@@ -0,0 +1,31 @@
+;; elfeed settings
+
+; feed configuration
+(setq  elfeed-feeds
+       '("http://feeds.washingtonpost.com/rss/national"
+        "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+        "http://feeds.arstechnica.com/arstechnica/index.xml"
+        "http://feeds.washingtonpost.com/rss/politics"
+        ))
+
+; helper functions
+
+(defun eww-elfeed-entry ()
+  (interactive)
+  (let ((link (elfeed-entry-link elfeed-show-entry)))
+  (switch-to-buffer-other-window (get-buffer-create "*eww*"))
+  (eww link)))
+
+; set up evil keybindings
+(evil-define-key 'normal elfeed-search-mode-map (kbd "r") 'elfeed-search-fetch)
+(evil-define-key 'normal elfeed-search-mode-map (kbd "R") 'elfeed-search-fetch)
+(evil-define-key 'normal elfeed-search-mode-map (kbd "d") 'elfeed-search-show-entry)
+(evil-define-key 'normal elfeed-search-mode-map (kbd "s") 'elfeed-search-live-filter)
+(evil-define-key 'normal elfeed-search-mode-map (kbd "q") 'evil-delete-buffer)
+(evil-define-key 'normal elfeed-show-mode-map (kbd "n") 'elfeed-show-next)
+(evil-define-key 'normal elfeed-show-mode-map (kbd "e") 'eww-elfeed-entry)
+(evil-define-key 'normal elfeed-show-mode-map (kbd "q") 'evil-delete-buffer)
+(evil-define-key 'normal elfeed-show-mode-map (kbd "u") '(lambda () (interactive) (switch-to-buffer "*elfeed-search*")))
+
+; disable images in elfeed-entry pages
+(setq shr-inhibit-images t)
