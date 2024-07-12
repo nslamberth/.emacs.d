12679f70165c5162aa434e2aaf37e0b23579b395
Nick Lamberth <nslamberth@gmail.com>
2024-07-12 09:26:27 -0400

    custom sync

--- custom.el
+++ custom.el
@@ -6,6 +6,8 @@
  ;; If there is more than one, they won't work right.
  '(auto-revert-verbose nil)
  '(byte-compile-warnings nil)
+ '(cape-dabbrev-check-other-buffers nil)
+ '(cape-dabbrev-min-length 2)
  '(completion-auto-select 'second-tab)
  '(custom-enabled-themes '(tsdh-dark))
  '(delete-selection-mode t)
@@ -14,19 +16,25 @@
  '(duplicate-line-final-position -1)
  '(electric-pair-mode t)
  '(embark-verbose-indicator-display-action '(display-buffer-at-bottom))
+ '(global-undo-tree-mode t)
  '(help-window-select t)
  '(hippie-expand-try-functions-list
    '(try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol try-expand-line))
  '(hledger-currency-string "$")
+ '(ignored-local-variable-values
+   '((Package . HUNCHENTOOT)
+     (Base . 10)
+     (Package . CL-WHO)
+     (Syntax . COMMON-LISP)))
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(isearch-wrap-pause 'no-ding)
  '(ledger-reports
-   '(("bank+credit card balance" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger bal assets liabilities and not loans -e \"today\"")
+   '(("bank and credit card register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger -p \"last month\" reg \\(assets or liabilities\\) and not \\(loans or house or 401k or vanguardira\\) -S date")
+     ("balance income and expenses" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger bal income expenses loans -p \"this month\"")
+     ("bank+credit card balance" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger  -e \"tomorrow\" bal \\(assets or liabilities\\) and not \\(loans or house or 401k or vanguardira\\)")
      ("visa register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger reg visa")
      ("asset register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger reg assets")
-     ("balance income and expenses" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger bal income expenses")
-     ("bank and credit card register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger reg assets liabilities and not loans")
      ("credit register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger reg visa")
      ("asset register" "ledger [[ledger-mode-flags]] -f /home/nicklamberth/Sync/projects/ledger/main.ledger reg assets")
      ("bal" "%(binary) -f %(ledger-file) bal")
@@ -51,7 +59,7 @@
  '(org-startup-truncated nil)
  '(org-use-speed-commands t)
  '(package-selected-packages
-   '(ledger-mode embark-consult embark sql-indent request goto-chg org-jira drag-stuff markdown-mode yaml-mode jump-char puni expand-region consult ace-window avy try orderless vertico multiple-cursors yasnippet-snippets which-key use-package undo-tree olivetti magit dumb-jump))
+   '(cape corfu-terminal corfu quelpa iedit ledger-mode embark-consult embark sql-indent request goto-chg org-jira drag-stuff markdown-mode yaml-mode jump-char puni expand-region consult ace-window avy try orderless vertico multiple-cursors yasnippet-snippets which-key use-package undo-tree olivetti magit dumb-jump))
  '(repeat-mode t)
  '(ring-bell-function 'ignore)
  '(scroll-bar-mode nil)
@@ -65,7 +73,8 @@
  '(tool-bar-mode nil)
  '(truncate-lines nil)
  '(truncate-partial-width-windows nil)
- '(undo-tree-enable-undo-in-region t)
+ '(undo-tree-auto-save-history nil)
+ '(undo-tree-enable-undo-in-region nil)
  '(view-read-only t)
  '(window-combination-resize t)
  '(winner-mode t)
@@ -76,4 +85,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completions-annotations ((t (:inherit shadow))))
  '(ledger-font-xact-highlight-face ((t (:inherit nil :extend t)))))
